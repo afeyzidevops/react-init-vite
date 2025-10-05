64bce07e34894597052b74e37bb56a1c8d4290d1
Ali Feyzi <a.feyzi.devops@gmail.com>
2025-10-05 18:20:28 +0330

    initialize project and use create list by direct add to <ui>

---
+++ src/components/List.jsx
@@ -0,0 +1,16 @@
+
+
+const Main_list = ()=>{
+   return(
+    <ul>
+        <li>داشبورد</li>
+        <li>گزارشات</li>
+        <li>نمودارها و ترندها</li>
+        <li>مشخصات فنی تجهیزات</li>
+        <li>نقشه های تک خطی</li>
+    </ul>
+   );
+};
+
+export default Main_list;
+
