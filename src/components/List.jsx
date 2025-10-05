cb51f3ff8f187be31d655348103603d83511ae34
Ali Feyzi <a.feyzi.devops@gmail.com>
2025-10-05 18:37:58 +0330

    Video-252:fragment (06:00') use <> </> insted fragment

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,17 +1,16 @@
 
-import { Fragment } from "react";
 
 const Main_list = ()=>{
 const menu_list=["داشبورد", "گزارشات" ,"نمودارها و ترندها" , "مشخصات فنی تجهیزات"  , "نقشه های تک خطی"] ;
    return(
-      <Fragment>
+      <>
       <h2>Menu bar</h2>
     <ul>
        {menu_list.map((item) => {
             return <li key={item}>{item}</li>
        })}
     </ul>
-    </Fragment>
+    </>
    );
 };
 export default Main_list;
