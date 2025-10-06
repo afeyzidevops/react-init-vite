5f4610b296283b42ff52dddcbd2bdc7cef972487
Ali Feyzi <a.feyzi.devops@gmail.com>
2025-10-06 14:26:45 +0330

    ##### video 253 (12:00) - use extrenal function of jsx and pass to jsx  to complate list #####

--- src/components/list_pc.jsx
+++ src/components/list_pc.jsx
@@ -2,17 +2,21 @@ const Main_list = ()=>{
 const menu_list=["داشبورد", "گزارشات" ,"نمودارها و ترندها" , "مشخصات فنی تجهیزات"  , "نقشه های تک خطی"] ;
    // if (!menu_list.length) return <div>this is a empty list</div>
    
+
+   const RenderList =() => {
+    if (!menu_list.length) return <div>this list is empety</div>;
+    return menu_list.map((item) =>(
+       <li key={item}>{item}</li>
+    )); 
+   };
+
+
    return(
-      <>
-      <h2>Menu bar</h2>
-      {/* {!menu_list.length ? <div>This is empty list</div> : null} */}
-      {/* use comprasion operators*/}
-      {!menu_list.length && <div>this is list is empty</div>}
-    <ul>
-       {menu_list.map((item) => {
-            return <li key={item}>{item}</li>
-       })}
-    </ul>
+    <>
+        <h2>Menu bar</h2>
+        <ul>
+        {RenderList()}
+        </ul>
     </>
    );
 };
