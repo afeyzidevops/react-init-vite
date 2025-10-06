14d740516c56226b9419c85e52b54c065ca0cfd0
Ali Feyzi <a.feyzi.devops@gmail.com>
2025-10-06 13:38:07 +0330

    replace list by list_pc

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Main_list from './components/list'
+import Main_list from './components/list_pc'
 
 function App() {
   const [count, setCount] = useState(0)
---
+++ src/components/list_pc.jsx
@@ -0,0 +1,19 @@
+const Main_list = ()=>{
+const menu_list=["داشبورد", "گزارشات" ,"نمودارها و ترندها" , "مشخصات فنی تجهیزات"  , "نقشه های تک خطی"] ;
+   // if (!menu_list.length) return <div>this is a empty list</div>
+   
+   return(
+      <>
+      <h2>Menu bar</h2>
+      {!menu_list.length ? <div>This is empty list</div> : null}
+      {/* use comprasion operators*/}
+      {/* {!menu_list.length && <div>this is list is empty</div>} */}
+    <ul>
+       {menu_list.map((item) => {
+            return <li key={item}>{item}</li>
+       })}
+    </ul>
+    </>
+   );
+};
+export default Main_list;
