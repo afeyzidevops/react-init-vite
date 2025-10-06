bbe5732c438715740e8424cf307d9f9b8a1de2f2
Ali Feyzi <a.feyzi.devops@gmail.com>
2025-10-06 10:54:53 +0330

    ##### Video 253 (04:00) use condition in jsx by ternery operator ######

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,9 +2,11 @@
 
 const Main_list = ()=>{
 const menu_list=["داشبورد", "گزارشات" ,"نمودارها و ترندها" , "مشخصات فنی تجهیزات"  , "نقشه های تک خطی"] ;
+   // if (!menu_list.length) return <div>this is a empty list</div>
    return(
       <>
       <h2>Menu bar</h2>
+      {!menu_list.length ? <div>This is empty list</div> : null}
     <ul>
        {menu_list.map((item) => {
             return <li key={item}>{item}</li>
